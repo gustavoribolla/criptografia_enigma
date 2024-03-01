f2e4f8894657dbe6f50a1b40527adf0331dc427b
Gustavo <gustavocr2@al.insper.edu.br>
2024-03-01 14:27:06 -0300

    Início dos testes --> Demo

--- demo.py
+++ demo.py
@@ -1,13 +1,19 @@
 from functions import *
 
-mensagem = "spfc"
 P = np.roll(np.eye(26, dtype=int), 1, axis=0)
+E = np.roll(np.eye(26, dtype=int), 2, axis=1)
+
+mensagem = "spfc"
+print(f"Mensagem Original: {mensagem}")
+
+matriz = para_one_hot(mensagem)
+print(matriz)
+
+mensagem = para_string(matriz)
+print(f"Mensagem em String: {mensagem}")
+
 mensagem_cifrada = cifrar(mensagem, P)
 print("Mensagem cifrada:", mensagem_cifrada)
 
-P = np.roll(np.eye(26, dtype=int), 1, axis=0)
 mensagem_original = de_cifrar(mensagem_cifrada, P)
 print("Mensagem decifrada:", mensagem_original)
-
-cifra = np.roll(np.eye(26, dtype=int), 1, axis=1)
-e = np.roll(np.eye(26, dtype=int), 2, axis=1)
